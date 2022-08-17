6147796932c2b160faa1b0264f6691336cb528d1
Jean <acalto2@gmail.com>
2022-08-17 22:40:47 +0200

    ok

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,13 @@ async function getCountry(country: string) {
       `https://restcountries.com/v2/name/${country}`
     );
     const data = getApi.data;
-    const countries = [];
-    for (let i = 0; i < data.length; i++) {
-      countries.push(data[i].name);
-    }
-    return countries;
+    const basicData = {
+      capital: data[0].capital,
+      region: data[0].region,
+      numericCode: data[0].numericCode
+    };
+   // console.log(basicData);
+    return basicData;
   }
 
 /** Create a test for this getRegion function */
@@ -34,10 +36,21 @@ async function getRegionCountries(regionalbloc: string) {
 }
 
 /** Create getRegionCapitals function here */
-
+async function getRegionCapitals(regionalbloc: string) {
+  const getApi = await axios(
+    `https://restcountries.com/v2/regionalbloc/${regionalbloc}`
+  );
+  const data = getApi.data
+  const regionCapitals = [];
+  for (let i = 0; i < data.length; i++) {
+    regionCapitals.push(data[i].capital);
+  }
+  return regionCapitals;
+}
 
 export default {
   getCountry,
   getRegionCountries,
   getRegionCapitals
 };
+
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -10,7 +10,12 @@ it("should get basic data on the country canada", async () => {
 });
 
 /** Add test for getRegionCountries function here */
-
+it("should get countries of NAFTA regional block", async () => {
+  const data = await countries.getRegionCountries('nafta');
+  expect(data).toEqual([
+    'Canada', 'Mexico', 'United States of America'
+  ]);
+});
 
 it("should get capitals of NAFTA countries", async () => {
   const data = await countries.getRegionCapitals('nafta');
